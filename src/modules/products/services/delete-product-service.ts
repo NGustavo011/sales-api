816d5ca22c01c8a9d25db4eb03d6307bb7c983bd
NGustavo011 <gsn181200@gmail.com>
2023-07-21 14:12:43 -0300

    feat: add delete-product-service

---
+++ src/modules/products/services/delete-product-service.ts
@@ -0,0 +1,18 @@
+import { getCustomRepository } from 'typeorm'
+import { ProductRepository } from '../typeorm/repositories/product-repository'
+import { AppError } from '@shared/errors/app-error'
+
+interface IRequest {
+  id: number
+}
+
+export class DeleteProductService {
+  public async execute ({ id }: IRequest): Promise<void> {
+    const productRepository = getCustomRepository(ProductRepository)
+    const product = await productRepository.findOne(id)
+    if (!product) {
+      throw new AppError('Product not found')
+    }
+    await productRepository.remove(product)
+  }
+}
