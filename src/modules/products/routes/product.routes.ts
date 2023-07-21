969d11b95e32d1cf0bb3937c54c751ffefed7a64
NGustavo011 <gsn181200@gmail.com>
2023-07-21 14:59:34 -0300

    feat: add celebrate validations in product routes

--- src/modules/products/routes/product.routes.ts
+++ src/modules/products/routes/product.routes.ts
@@ -1,11 +1,51 @@
 import { Router } from 'express'
 import { ProductController } from '../controllers/product-controller'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 export const productsRouter = Router()
 
 const productController = new ProductController()
 productsRouter.get('/', productController.index)
-productsRouter.get('/:id', productController.show)
-productsRouter.post('/', productController.create)
-productsRouter.put('/:id', productController.update)
-productsRouter.delete('/:id', productController.delete)
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required()
+    }
+  }),
+  productController.show
+)
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    }
+  }),
+  productController.create
+)
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required()
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    }
+  }),
+  productController.update
+)
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required()
+    }
+  }),
+  productController.delete
+)
