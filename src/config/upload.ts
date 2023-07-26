e0975ab060b28a33296e661da6f35dde519bf4b8
NGustavo011 <gsn181200@gmail.com>
2023-07-26 15:22:37 -0300

    feat: add disk storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,10 +3,12 @@ import path from 'path'
 import crypto from 'crypto'
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads')
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp')
 export const uploadConfig = {
   directory: uploadFolder,
+  tmpFolder,
   storage: multer.diskStorage({
-    destination: uploadFolder,
+    destination: tmpFolder,
     filename (request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex')
       const fileName = `${fileHash}-${file.originalname}`
--- src/modules/users/services/update-user-avatar-service.ts
+++ src/modules/users/services/update-user-avatar-service.ts
@@ -1,10 +1,8 @@
 import { AppError } from '@shared/errors/app-error'
 import { UserRepository } from '../typeorm/repositories/user-repository'
 import { getCustomRepository } from 'typeorm'
-import path from 'path'
-import { uploadConfig } from '@config/upload'
-import fs from 'fs'
 import { type User } from '../typeorm/entities/user'
+import { DiskStorageProvider } from '@shared/providers/storage-provider/disk-storage-provider'
 
 interface IRequest {
   userId: number
@@ -14,18 +12,16 @@ interface IRequest {
 export class UpdateUserAvatarService {
   public async execute ({ userId, avatarFileName }: IRequest): Promise<User> {
     const userRepository = getCustomRepository(UserRepository)
+    const storageProvider = new DiskStorageProvider()
     const user = await userRepository.findOne(userId)
     if (!user) {
       throw new AppError('User not found')
     }
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await storageProvider.deleteFile(user.avatar)
     }
-    user.avatar = avatarFileName
+    const fileName = await storageProvider.saveFile(avatarFileName)
+    user.avatar = fileName
     await userRepository.save(user)
     return user
   }
